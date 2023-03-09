926b2acc2d5ff9b3a84ece26c3579a48ea9175a0
Igor <imartynkin@yandex.ru>
2023-03-10 00:04:48 +0900

    add branch for auth DAO Authentication

--- src/main/java/ru/martynkin/security/controllers/MainUserController.java
+++ src/main/java/ru/martynkin/security/controllers/MainUserController.java
@@ -1,7 +1,6 @@
 package ru.martynkin.security.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
