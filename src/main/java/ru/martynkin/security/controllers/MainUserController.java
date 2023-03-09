03311945527bc7b7e187bba7c21e4a60c5107ec2
Igor <imartynkin@yandex.ru>
2023-03-10 00:04:31 +0900

    add branch for auth DAO Authentication

--- src/main/java/ru/martynkin/security/controllers/MainUserController.java
+++ src/main/java/ru/martynkin/security/controllers/MainUserController.java
@@ -1,25 +1,46 @@
 package ru.martynkin.security.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.martynkin.security.entity.User;
+import ru.martynkin.security.services.UserService;
 
 import java.security.Principal;
 
 @RestController
 public class MainUserController {
 
+    private UserService userService;
+
+    @Autowired
+    public MainUserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/")
     public String homePage() {
         return "homePage";
     }
 
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @GetMapping("/admin")
     public String adminPage(Principal principal) {
-        return "adminPage, user: " + principal.getName();
+        User user = userService.findByUsername(principal.getName());
+        return "adminPage, user: " + principal.getName() + "\n"
+                + ", email: " + user.getEmail() + "\n"
+                + ", password: " + user.getPassword();
     }
 
     @GetMapping("/pageAdminRole")
     public String pageAdminRole(Principal principal) {
-        return "pageAdminRole, user: " + principal.getName();
+        User user = userService.findByUsername(principal.getName());
+        return "pageAdminRole, user: " + principal.getName() + ", email: " + user.getEmail();
     }
 }
