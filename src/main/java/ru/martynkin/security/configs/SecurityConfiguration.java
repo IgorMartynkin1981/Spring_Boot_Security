58abd943a6a4af2e0d1647e99bfc2d9fec85a6b7
Igor <imartynkin@yandex.ru>
2023-03-09 21:37:46 +0900

    add branch for auth from token

--- src/main/java/ru/martynkin/security/configs/SecurityConfiguration.java
+++ src/main/java/ru/martynkin/security/configs/SecurityConfiguration.java
@@ -2,20 +2,21 @@ package ru.martynkin.security.configs;
 
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
-@Configuration
+/**
+ * JDBC Authentication
+ */
+
+@EnableWebSecurity
 public class SecurityConfiguration {
 
     @Bean
@@ -30,33 +31,6 @@ public class SecurityConfiguration {
 
         return http.build();
     }
-
-    //In-Memory Authentication
-//    @Bean
-//    public InMemoryUserDetailsManager userDetailsService() {
-//        UserDetails user = User.builder()
-//                .username("user")
-//                //пароль password
-//                .password("{bcrypt}$2a$12$tHyc4gCrOkcQz5DLGLHz1.U3xV7YQDchnsoPwJ2n0TsT12mqiw/Bi")
-//                .roles("USER")
-//                .build();
-//        UserDetails admin = User.builder()
-//                .username("user")
-//                //пароль password
-//                .password("{bcrypt}$2a$12$tHyc4gCrOkcQz5DLGLHz1.U3xV7YQDchnsoPwJ2n0TsT12mqiw/Bi")
-//                .roles("ADMIN", "USER")
-//                .build();
-//        return new InMemoryUserDetailsManager(user, admin);
-//    }
-
-    //JDBC Authentication
-//    @Bean
-//    public DataSource dataSource() {
-//        return new EmbeddedDatabaseBuilder()
-//                .setType(EmbeddedDatabaseType.H2)
-//                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
-//                .build();
-//    }
 
     @Bean
     public UserDetailsManager users(DataSource dataSource) {
