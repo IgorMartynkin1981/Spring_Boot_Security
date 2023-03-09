53a3d1517f0a3ef6b697e151f136e414d0456e6e
Igor <imartynkin@yandex.ru>
2023-03-09 21:40:43 +0900

    add branch for auth In-Memory Authentication

--- src/main/java/ru/martynkin/security/configs/SecurityConfiguration.java
+++ src/main/java/ru/martynkin/security/configs/SecurityConfiguration.java
@@ -2,20 +2,18 @@ package ru.martynkin.security.configs;
 
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.sql.DataSource;
+/**
+ * In-Memory Authentication
+ */
 
-@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 
     @Bean
@@ -31,35 +29,8 @@ public class SecurityConfiguration {
         return http.build();
     }
 
-    //In-Memory Authentication
-//    @Bean
-//    public InMemoryUserDetailsManager userDetailsService() {
-//        UserDetails user = User.builder()
-//                .username("user")
-//                //пароль password
-//                .password("{bcrypt}$2a$12$tHyc4gCrOkcQz5DLGLHz1.U3xV7YQDchnsoPwJ2n0TsT12mqiw/Bi")
-//                .roles("USER")
-//                .build();
-//        UserDetails admin = User.builder()
-//                .username("user")
-//                //пароль password
-//                .password("{bcrypt}$2a$12$tHyc4gCrOkcQz5DLGLHz1.U3xV7YQDchnsoPwJ2n0TsT12mqiw/Bi")
-//                .roles("ADMIN", "USER")
-//                .build();
-//        return new InMemoryUserDetailsManager(user, admin);
-//    }
-
-    //JDBC Authentication
-//    @Bean
-//    public DataSource dataSource() {
-//        return new EmbeddedDatabaseBuilder()
-//                .setType(EmbeddedDatabaseType.H2)
-//                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
-//                .build();
-//    }
-
     @Bean
-    public UserDetailsManager users(DataSource dataSource) {
+    public InMemoryUserDetailsManager userDetailsService() {
         UserDetails user = User.builder()
                 .username("user")
                 //пароль password
@@ -67,20 +38,11 @@ public class SecurityConfiguration {
                 .roles("USER")
                 .build();
         UserDetails admin = User.builder()
-                .username("admin")
+                .username("user")
                 //пароль password
                 .password("{bcrypt}$2a$12$tHyc4gCrOkcQz5DLGLHz1.U3xV7YQDchnsoPwJ2n0TsT12mqiw/Bi")
                 .roles("ADMIN", "USER")
                 .build();
-        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
-        if (users.userExists(user.getUsername())) {
-            users.deleteUser(user.getUsername());
-        }
-        if (users.userExists(admin.getUsername())) {
-            users.deleteUser(admin.getUsername());
-        }
-        users.createUser(user);
-        users.createUser(admin);
-        return users;
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
