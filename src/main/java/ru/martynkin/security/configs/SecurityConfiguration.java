11bfadffe670d7f2a6f18774bf0d39325e316b19
Igor <imartynkin@yandex.ru>
2023-03-09 22:09:39 +0900

    add branch for auth Default Authentication

--- src/main/java/ru/martynkin/security/configs/SecurityConfiguration.java
+++ src/main/java/ru/martynkin/security/configs/SecurityConfiguration.java
@@ -4,13 +4,10 @@ package ru.martynkin.security.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
- * In-Memory Authentication
+ * Default Authentication
  */
 
 @EnableWebSecurity
@@ -21,29 +18,11 @@ public class SecurityConfiguration {
         http
                 .authorizeRequests()
                 .antMatchers("/admin/**").authenticated()
-                .antMatchers("/pageAdminRole/**").hasRole("ADMIN")
                 .and()
                 .formLogin()
                 .and()
                 .logout().logoutSuccessUrl("/");
 
         return http.build();
-    }
-
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.builder()
-                .username("user")
-                //пароль password
-                .password("{bcrypt}$2a$12$tHyc4gCrOkcQz5DLGLHz1.U3xV7YQDchnsoPwJ2n0TsT12mqiw/Bi")
-                .roles("USER")
-                .build();
-        UserDetails admin = User.builder()
-                .username("admin")
-                //пароль password
-                .password("{bcrypt}$2a$12$tHyc4gCrOkcQz5DLGLHz1.U3xV7YQDchnsoPwJ2n0TsT12mqiw/Bi")
-                .roles("ADMIN", "USER")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
     }
 }
